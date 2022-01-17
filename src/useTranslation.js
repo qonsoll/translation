b63df9ea195bf94025e1437f6a17bbd893f772de
Maxim Makarov <maxim.makarov@senseteq.io>
2022-01-17 13:04:18 +0200

    fix firebase dependency version, add default useTransaction return

--- src/useTranslation.js
+++ src/useTranslation.js
@@ -1,6 +1,14 @@
 import { useContext } from 'react'
 import TranslationContext from './TranslationContext'
 
-const useTranslation = () => useContext(TranslationContext)
+const useTranslation = () => {
+  const context = useContext(TranslationContext)
+
+  if (context === undefined) {
+    return { t: (text) => text }
+  }
+
+  return context
+}
 
 export default useTranslation
